69df78c6c98794e2fe4b2a36cfd8640495cf77ac
chichileishvili <giochichileishvili20@gmail.com>
2025-01-16 18:05:18 +0400

    refactor: Update package.json to include @types/node and exports for ES modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
   build: {
     lib: {
       entry: "src/index.ts",
       name: "SuadaComponents",
       fileName: (format) => `suada-components.${format}.js`,
+      formats: ["es", "cjs", "umd"],
     },
     rollupOptions: {
-      // Ensure to externalize dependencies that shouldn't be bundled
       external: ["react", "react-dom"],
       output: {
         globals: {
