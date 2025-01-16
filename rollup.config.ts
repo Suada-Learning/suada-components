f1d1fde0b92956ac38812775e664c02b03aea786
chichileishvili <giochichileishvili20@gmail.com>
2025-01-16 17:47:15 +0400

    refactor: Update package.json to include @rollup/plugin-babel, @rollup/plugin-commonjs, and @rollup/plugin-node-resolve

---
+++ rollup.config.ts
@@ -0,0 +1,31 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    babel({
+      exclude: "node_modules/**",
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+      babelHelpers: "bundled",
+    }),
+  ],
+  external: ["react", "react-dom"],
+};
