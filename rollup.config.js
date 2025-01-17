0c53d5596d6691eaf379bdc62864268e4c6581f4
chichileishvili <giochichileishvili20@gmail.com>
2025-01-17 14:55:06 +0400

    refactor: Update rollup config

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import babel from "@rollup/plugin-babel";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    postcss({
+      modules: true,
+      extract: true,
+      minimize: true,
+    }),
+    typescript({
+      tsconfig: "./tsconfig.json",
+      declaration: true,
+      declarationDir: "dist/types",
+      exclude: ["**/*.stories.tsx", "**/*.test.tsx"],
+    }),
+    babel({
+      exclude: "node_modules/**",
+      babelHelpers: "bundled",
+      extensions: [".ts", ".tsx"],
+    }),
+  ],
+  external: ["react", "react-dom"],
+};
