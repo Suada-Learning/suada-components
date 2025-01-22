e4be46efd945ae5164a21bb8d86512a3ca620b68
chichileishvili <giochichileishvili20@gmail.com>
2025-01-22 16:26:55 +0400

    refactor: Update package.json and rollup.config.js for component relocation and export paths

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import postcss from "rollup-plugin-postcss";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: "index.ts",
+  input: "./src/index.ts",
   output: [
     {
       file: "dist/index.js",
