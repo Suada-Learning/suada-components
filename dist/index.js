ba83264d77df6b367134ecb8a2359c36251c4a41
chichileishvili <giochichileishvili20@gmail.com>
2025-02-11 14:06:07 +0400

    fix: svg imports

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,6 @@ export { GridActionMenu } from './components/GridActionMenu';
 export { SearchInput } from './components/SearchInput';
 export { TranslationProvider } from './components/Providers/TranslationProvider';
 export { ThemeProvider } from './components/Providers/ThemeProvider';
-export { ActivityIcon } from './svg/Activity';
-export { AddAlt } from './svg/AddAlt';
+export { ActivityIcon } from './svg';
+export { AddAlt } from './svg';
 //# sourceMappingURL=index.js.map
