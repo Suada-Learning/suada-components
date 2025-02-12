2446ed2cc367d785d7280f7d2f6835900fb4ebcf
chichileishvili <giochichileishvili20@gmail.com>
2025-02-13 01:50:14 +0400

    feat: add customSwitcher component story

---
+++ dist/types/components/Switcher/CustomSwitcher.interface.d.ts
@@ -0,0 +1,12 @@
+import { ChangeEvent } from 'react';
+export interface ISwitchProps {
+    value: boolean;
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    name?: string;
+    label?: string;
+    off?: string | React.ReactNode;
+    on?: string | React.ReactNode;
+    tooltip?: string;
+    labelPosition?: 'start' | 'end';
+    labelStyle?: React.CSSProperties;
+}
