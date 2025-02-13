de10356a33e2a8a3d4af362f9d1cc04691a897bf
levani-k <levaniqistauri@gmail.com>
2025-02-13 11:37:54 +0400

    chore: update Storybook documentation and assets

--- docs/assets/GridActionMenu.stories-CglH0D6l.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./jsx-runtime-1u1Nfn-y.js";import{G as d,C as u,a as s}from"./CourseCardActionsConfig-CqmfN_aR.js";import{G as l}from"./global.styles-LHF88hKn.js";import{C as p}from"./ThemeProvider-QavAmAtN.js";import"./index-DmTNtw1v.js";import"./Theme-DB1c5mYq.js";import"./index-7tnRfIdv.js";import"./index-BJ6hxVbY.js";const i={id:1,isBought:!1,video:!0},c={actionHandler:(o,m)=>console.log(o,m)},h={title:"Components/GridActionMenu",component:d,tags:["autodocs"],decorators:[o=>e.jsxs(p,{children:[e.jsx(l,{}),e.jsx(u,{children:e.jsx(o,{})})]})],parameters:{layout:"centered"},args:{actionConfig:s(c.actionHandler,{deleteCourse:!0},o=>o),row:i}},t={args:{actionConfig:s(c.actionHandler,{deleteCourse:!0},o=>o),row:i}};var r,n,a;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    actionConfig: actionConfig(mockFunctions.actionHandler, {
-      deleteCourse: true
-    }, key => key),
-    row: mockRow
-  }
-}`,...(a=(n=t.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const y=["Default"];export{t as Default,y as __namedExportsOrder,h as default};
