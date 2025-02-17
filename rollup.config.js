1a346d883b9df9749aee044cfc6e1e2a684e4e5d
levani-k <levaniqistauri@gmail.com>
2025-02-17 12:07:20 +0400

    feat: update package configuration for icons export

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,51 @@
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
-import babel from "@rollup/plugin-babel";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
-import commonjs from "@rollup/plugin-commonjs";
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import babel from '@rollup/plugin-babel'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
-  input: "./src/index.ts",
+  input: {
+    index: 'src/index.ts',
+    icons: 'src/icons.ts',
+  },
   output: [
     {
-      file: "dist/index.js",
-      format: "cjs",
+      dir: 'dist',
+      format: 'cjs',
       sourcemap: true,
+      entryFileNames: '[name].js',
     },
     {
-      file: "dist/index.esm.js",
-      format: "esm",
+      dir: 'dist',
+      format: 'esm',
       sourcemap: true,
+      entryFileNames: '[name].esm.js',
     },
   ],
   plugins: [
     peerDepsExternal(),
-
     resolve(),
-
     commonjs({
       include: /node_modules/,
     }),
-
     postcss({
       modules: true,
       extract: true,
       minimize: true,
     }),
-
     typescript({
-      tsconfig: "./tsconfig.json",
+      tsconfig: './tsconfig.json',
       declaration: true,
-      declarationDir: "dist/types",
-      exclude: ["**/*.stories.tsx", "**/*.test.tsx"],
+      declarationDir: 'dist/types',
+      exclude: ['**/*.stories.tsx', '**/*.test.tsx'],
     }),
-
     babel({
-      exclude: "node_modules/**",
-      babelHelpers: "bundled",
-      extensions: [".ts", ".tsx"],
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled',
+      extensions: ['.ts', '.tsx'],
     }),
   ],
-  external: ["react", "react-dom"],
-};
+  external: ['react', 'react-dom'],
+}
