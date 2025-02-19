0d6f34e5a5fa6d7a36ed5000b9412768572da0f2
chichileishvili <giochichileishvili20@gmail.com>
2025-02-20 03:38:12 +0400

    fix: rename docs

--- docs/assets/GridActionMenu.stories-SC7Ga3lP.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./jsx-runtime-Be5KepTg.js";import{G as p,C as d,a}from"./CourseCardActionsConfig-BsihBP54.js";import{G as u}from"./global.styles-CLsqfKF_.js";import{C as l}from"./ThemeProvider-D78950lj.js";import"./index-Cu3ntQMU.js";import"./index-BsICj7Do.js";import"./createTheme-BTfVaJ7I.js";import"./DownloadIcon-CXLA5aQr.js";import"./Popover-DFaBvpDN.js";import"./useSlot-BEsZemBB.js";import"./DefaultPropsProvider-BB0hjY0t.js";import"./index-cfNomcto.js";import"./index-DYvH7Hsd.js";import"./Grow-CXQGBCIP.js";import"./Paper-5RdbgvWt.js";import"./Typography-onE-EyQr.js";import"./createSimplePaletteValueFilter-pjaBuWeA.js";import"./Theme-CXUkqRU4.js";const s={id:1,isBought:!1,video:!0},m={actionHandler:(o,c)=>console.log(o,c)},S={title:"Components/GridActionMenu",component:p,tags:["autodocs"],decorators:[o=>t.jsxs(l,{children:[t.jsx(u,{}),t.jsx(d,{children:t.jsx(o,{})})]})],parameters:{layout:"centered"},args:{actionConfig:a(m.actionHandler,{deleteCourse:!0},o=>o),row:s}},r={args:{actionConfig:a(m.actionHandler,{deleteCourse:!0},o=>o),row:s}};var e,n,i;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    actionConfig: actionConfig(mockFunctions.actionHandler, {
-      deleteCourse: true
-    }, key => key),
-    row: mockRow
-  }
-}`,...(i=(n=r.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};const _=["Default"];export{r as Default,_ as __namedExportsOrder,S as default};
