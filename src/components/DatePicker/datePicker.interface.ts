82d854c30b18cb8792a4427260079691d3cde559
chichileishvili <giochichileishvili20@gmail.com>
2025-02-20 15:15:13 +0400

    feat: add DatePicker component with styled components and interface

---
+++ src/components/DatePicker/datePicker.interface.ts
@@ -0,0 +1,11 @@
+export interface IDatePickerProps {
+  error?: boolean
+  errorMessage?: string
+  name?: string
+  label?: string
+  touched?: boolean
+  value?: Date | string | null
+  onChange?: (date: Date | null) => void
+  styles?: React.CSSProperties
+  format?: string
+}
