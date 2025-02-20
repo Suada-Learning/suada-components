df89bccc438d4eacd166874333e41337113f1375
chichileishvili <giochichileishvili20@gmail.com>
2025-02-20 19:54:45 +0400

    feat: add Checkbox component with customizable label and tooltip

---
+++ src/components/CheckboxV2/Checkbox.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { StoryFn, Meta } from '@storybook/react'
+import { Checkbox } from './Checkbox'
+
+export default {
+  title: 'Components/CheckboxV2',
+  component: Checkbox,
+
+  tags: ['autodocs', 'checkbox'],
+  decorators: [
+    Story => (
+      <div
+        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}
+      >
+        <div style={{ maxWidth: '400px' }}>
+          <Story />
+        </div>
+      </div>
+    ),
+  ],
+
+  argTypes: {
+    checked: { control: 'boolean' },
+    label: { control: 'text' },
+    tooltip: { control: 'text' },
+  },
+} as Meta<typeof Checkbox>
+
+const Template: StoryFn<typeof Checkbox> = args => <Checkbox {...args} />
+
+export const Checked = Template.bind({})
+
+Checked.args = {
+  checked: true,
+  label: 'Checkbox Label',
+  tooltip: 'Tooltip Text',
+}
+
+export const Unchecked = Template.bind({})
+Unchecked.args = {
+  checked: false,
+  label: 'Checkbox Label',
+  tooltip: 'Tooltip Text',
+}
---
+++ src/components/CheckboxV2/checkboxV2.interface.ts
@@ -0,0 +1,9 @@
+import { ChangeEvent } from 'react'
+
+export interface ICheckboxV2Props {
+  checked: boolean
+  onChange: (e?: ChangeEvent<HTMLInputElement>) => void
+  label: string
+  tooltip?: string
+  name?: string
+}
