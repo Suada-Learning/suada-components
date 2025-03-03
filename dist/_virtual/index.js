b018e4d79ecd7f580b6b28c9640106500a695a6a
chichileishvili <giochichileishvili20@gmail.com>
2025-03-04 00:10:55 +0400

    feat: add virtual module exports for various libraries and components

---
+++ dist/_virtual/index.js
@@ -0,0 +1,4 @@
+import { __require as requireDist } from '../node_modules/react-router/node_modules/cookie/dist/index.js';
+
+requireDist();
+//# sourceMappingURL=index.js.map
--- rollup.config.js
+++ rollup.config.js
@@ -4,47 +4,38 @@ import babel from '@rollup/plugin-babel'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import commonjs from '@rollup/plugin-commonjs'
-import { readdirSync } from 'fs'
-import path from 'path'
+import multiInput from 'rollup-plugin-multi-input'
 
-const iconsPath = './src/icons'
-const iconsFiles = readdirSync(iconsPath)
-  .filter(file => file.endsWith('.tsx'))
-  .reduce((entries, file) => {
-    const name = path.basename(file, '.tsx')
-    entries[`icons/${name}`] = path.join(iconsPath, file)
-    return entries
-  }, {})
-
-export default {
-  input: {
-    index: './src/index.ts',
-    ...iconsFiles,
+export default [
+  {
+    input: ['src/index.ts', 'src/icons/index.ts'],
+    output: [
+      {
+        dir: 'dist',
+        format: 'esm',
+        sourcemap: true,
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs({ include: /node_modules/ }),
+      postcss({ modules: true, extract: true, minimize: true }),
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: 'dist/types',
+        exclude: ['**/*.stories.tsx', '**/*.test.tsx'],
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+        extensions: ['.ts', '.tsx'],
+      }),
+      multiInput(),
+    ],
+    external: ['react', 'react-dom'],
   },
-  output: [
-    {
-      dir: 'dist',
-      format: 'esm',
-      sourcemap: true,
-      entryFileNames: '[name].js',
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs({ include: /node_modules/ }),
-    postcss({ modules: true, extract: true, minimize: true }),
-    typescript({
-      tsconfig: './tsconfig.json',
-      declaration: true,
-      declarationDir: 'dist/types',
-      exclude: ['**/*.stories.tsx', '**/*.test.tsx'],
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      babelHelpers: 'bundled',
-      extensions: ['.ts', '.tsx'],
-    }),
-  ],
-  external: ['react', 'react-dom'],
-}
+]
